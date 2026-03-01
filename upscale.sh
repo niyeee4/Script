485343a224ce1d992fecb30d412213f563fd0d51
niyeee4 <niyee4@gmail.com>
2026-03-01 16:40:50 +0700

    Update upscale.sh

--- upscale.sh
+++ upscale.sh
@@ -1,24 +1,21 @@
-apt update && apt upgrade
-sudo apt update
-sudo apt install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt update
-sudo apt install -y python3.7 python3.7-distutils
-curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.7 get-pip.py
+sudo apt update && sudo apt upgrade
 git clone https://github.com/xinntao/Real-ESRGAN
 cd Real-ESRGAN
 apt install wget -y
 wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth -P experiments/pretrained_models
 wget https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.2.4/RealESRGAN_x4plus_anime_6B.pth -P weights
-pip3.7 install basicsr
-pip3.7 install facexlib
-pip3.7 install gfpgan
-pip3.7 install -r requirements.txt
-python3.7 setup.py develop
+pip uninstall torch torchvision torchaudio -y
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install basicsr
+pip install facexlib
+pip install gfpgan
+pip install -r requirements.txt --no-deps
+python setup.py develop
 
-sudo tee /usr/local/bin/upscale > /dev/null << 'EOF'
+cat << 'EOF' | sudo tee /usr/local/bin/upscale > /dev/null
 #!/bin/bash
+
+cd /content/Real-ESRGAN || exit
 
 INPUT="$1"
 
@@ -27,7 +24,7 @@ NAME="${BASENAME%.*}"
 
 OUTPUT="${NAME}_x4.mp4"
 
-python3.7 inference_realesrgan_video.py -i "$INPUT" -o "$OUTPUT" -n RealESRGAN_x4plus_anime_6B -s 4
+python inference_realesrgan_video.py -i "$INPUT" -o "$OUTPUT" -n RealESRGAN_x4plus_anime_6B -s 4
 
 echo "Done → $OUTPUT"
 EOF
@@ -35,4 +32,4 @@ EOF
 sudo chmod +x /usr/local/bin/upscale
 
 echo DONE
-echo "Use: !upscale "yourfile.mp4"
+echo 'Use: !upscale "yourfile.mp4"'
